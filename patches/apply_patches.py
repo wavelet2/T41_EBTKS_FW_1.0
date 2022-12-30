5da2d7ccfd3572ca28d03a77f8ade48911f766c7
Richard Steedman <steedman@localhost.localdomain>
2022-12-30 12:14:14 +0000

    Pass list of args rather than string to subprocess.Popen method

--- patches/apply_patches.py
+++ patches/apply_patches.py
@@ -5,11 +5,11 @@ import subprocess
 Import("env")
 
 # So that we can run the same patch command in either Windows or Linux,
-# we execute "python <path-to-current-dir>\patches\patch.py <patch-file>"
-# The Python patch program we are using comes from here:
-# https://github.com/techtonik/python-patch
-PATCH_CMD = "python " + join(getcwd(), "patches", "patch.py")
+# we use a program patch.py from here: https://github.com/techtonik/python-patch
+# Our copy is stored in the 'patches' subdirectory.
+PATCH_PROG = join(getcwd(), "patches", "patch.py")
 
+# Directories containing the files we want to patch
 FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-arduinoteensy")
 TEENSY4_DIR = join(FRAMEWORK_DIR, "cores", "teensy4")
 USBHOST_DIR = join(FRAMEWORK_DIR, "libraries", "USBHost_t36")
@@ -21,7 +21,8 @@ def _touch(path):
         fp.write("")
 
 # For each file, only patch if not already done
-# Each patch file requires us to cd to the directory containing the file to be patched before running patch.py
+# Each patch file requires us to cd to the directory containing the file to be
+# patched before running patch.py.  We use subprocess.Popen() to do this.
 
 # 1st patch - .platformio\packages\framework-arduinoteensy\cores\teensy4\usb.c
 file_to_patch = join(TEENSY4_DIR, "usb.c")
@@ -29,7 +30,7 @@ patch_file    = join(getcwd(), "patches", "usb.c.patch")
 doneflag_file = join(TEENSY4_DIR, "usb.c.patch-done")
 if not isfile(doneflag_file):
     assert isfile(file_to_patch) and isfile(patch_file)
-    p = subprocess.Popen(PATCH_CMD + " " + patch_file, cwd=TEENSY4_DIR)
+    p = subprocess.Popen(["python", PATCH_PROG, patch_file], cwd=TEENSY4_DIR)
     p.wait()
     env.Execute(lambda *args, **kwargs: _touch(doneflag_file))
 
@@ -39,7 +40,7 @@ patch_file    = join(getcwd(), "patches", "HardwareSerial.cpp.patch")
 doneflag_file = join(TEENSY4_DIR, "HardwareSerial.cpp.patch-done")
 if not isfile(doneflag_file):
     assert isfile(file_to_patch) and isfile(patch_file)
-    p = subprocess.Popen(PATCH_CMD + " " + patch_file, cwd=TEENSY4_DIR)
+    p = subprocess.Popen(["python", PATCH_PROG, patch_file], cwd=TEENSY4_DIR)
     p.wait()
     env.Execute(lambda *args, **kwargs: _touch(doneflag_file))
 
@@ -49,7 +50,7 @@ patch_file    = join(getcwd(), "patches", "imxrt1062_t41.ld.patch")
 doneflag_file = join(TEENSY4_DIR, "imxrt1062_t41.ld.patch-done")
 if not isfile(doneflag_file):
     assert isfile(file_to_patch) and isfile(patch_file)
-    p = subprocess.Popen(PATCH_CMD + " " + patch_file, cwd=TEENSY4_DIR)
+    p = subprocess.Popen(["python", PATCH_PROG, patch_file], cwd=TEENSY4_DIR)
     p.wait()
     env.Execute(lambda *args, **kwargs: _touch(doneflag_file))
 
@@ -59,7 +60,7 @@ patch_file    = join(getcwd(), "patches", "ehci.cpp.patch")
 doneflag_file = join(USBHOST_DIR, "ehci.cpp.patch-done")
 if not isfile(doneflag_file):
     assert isfile(file_to_patch) and isfile(patch_file)
-    p = subprocess.Popen(PATCH_CMD + " " + patch_file, cwd=USBHOST_DIR)
+    p = subprocess.Popen(["python", PATCH_PROG, patch_file], cwd=USBHOST_DIR)
     p.wait()
     env.Execute(lambda *args, **kwargs: _touch(doneflag_file))
 
@@ -69,7 +70,7 @@ patch_file    = join(getcwd(), "patches", "SdFatConfig.h.patch")
 doneflag_file = join(SDFAT_DIR, "SdFatConfig.h.patch-done")
 if not isfile(doneflag_file):
     assert isfile(file_to_patch) and isfile(patch_file)
-    p = subprocess.Popen(PATCH_CMD + " " + patch_file, cwd=SDFAT_DIR)
+    p = subprocess.Popen(["python", PATCH_PROG, patch_file], cwd=SDFAT_DIR)
     p.wait()
     env.Execute(lambda *args, **kwargs: _touch(doneflag_file))
 
@@ -79,6 +80,6 @@ patch_file    = join(getcwd(), "patches", "Base64.cpp.patch")
 doneflag_file = join(BASE64_DIR, "Base64.cpp.patch-done")
 if not isfile(doneflag_file):
     assert isfile(file_to_patch) and isfile(patch_file)
-    p = subprocess.Popen(PATCH_CMD + " " + patch_file, cwd=BASE64_DIR)
+    p = subprocess.Popen(["python", PATCH_PROG, patch_file], cwd=BASE64_DIR)
     p.wait()
     env.Execute(lambda *args, **kwargs: _touch(doneflag_file))
